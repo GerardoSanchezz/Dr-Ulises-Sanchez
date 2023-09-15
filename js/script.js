455c60ee773ba864b36d082c77615f56a8198af2
Gerardo <gerardosanchezz14@hotmail.com>
2023-09-15 14:27:06 -0600

    Se corrige la nav bar

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
-// Obtén todos los enlaces de tu página
-const allLinks = document.querySelectorAll("a");
-const headerEl = document.querySelector(".header");
+// // Obtén todos los enlaces de tu página
+// const allLinks = document.querySelectorAll("a");
+// const headerEl = document.querySelector(".header");
 
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    const href = link.getAttribute("href");
+// allLinks.forEach(function (link) {
+//   link.addEventListener("click", function (e) {
+//     const href = link.getAttribute("href");
 
-    // Verifica si el enlace es un enlace interno (comienza con "#")
-    if (href.startsWith("#")) {
-      e.preventDefault(); // Previene el comportamiento predeterminado solo para enlaces internos
+//     // Verifica si el enlace es un enlace interno (comienza con "#")
+//     if (href.startsWith("#")) {
+//       e.preventDefault(); // Previene el comportamiento predeterminado solo para enlaces internos
 
-      // Scroll back to top o hacia otras secciones
-      if (href === "#") {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      } else {
-        const sectionEl = document.querySelector(href);
-        if (sectionEl) {
-          sectionEl.scrollIntoView({
-            behavior: "smooth",
-          });
-        }
-      }
+//       // Scroll back to top o hacia otras secciones
+//       if (href === "#") {
+//         window.scrollTo({
+//           top: 0,
+//           behavior: "smooth",
+//         });
+//       } else {
+//         const sectionEl = document.querySelector(href);
+//         if (sectionEl) {
+//           sectionEl.scrollIntoView({
+//             behavior: "smooth",
+//           });
+//         }
+//       }
 
-      // Cierra el menú de navegación móvil si está abierto
-      headerEl.classList.remove("nav-open");
-    } else {
-      // Si el enlace es externo, permite la navegación normal
-      // No es necesario hacer nada aquí
-    }
-  });
-});
+//       // Cierra el menú de navegación móvil si está abierto
+//       headerEl.classList.remove("nav-open");
+//     } else {
+//       // Si el enlace es externo, permite la navegación normal
+//       // No es necesario hacer nada aquí
+//     }
+//   });
+// });
+
 // console.log("Hello world!");
 
 // const myName = "Jonas Schmedtmann";
@@ -55,41 +56,41 @@ allLinks.forEach(function (link) {
 // ///////////////////////////////////////////////////////////
 // // Make mobile navigation work
 
-// const btnNavEl = document.querySelector(".btn-mobile-nav");
-// const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
 
-// btnNavEl.addEventListener("click", function () {
-//   headerEl.classList.toggle("nav-open");
-// });
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
-// const allLinks = document.querySelectorAll("a:link");
+const allLinks = document.querySelectorAll(".nav-link");
 
-// allLinks.forEach(function (link) {
-//   link.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     const href = link.getAttribute("href");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-//     // Scroll back to top
-//     if (href === "#")
-//       window.scrollTo({
-//         top: 0,
-//         behavior: "smooth",
-//       });
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
 
-//     // Scroll to other links
-//     if (href !== "#" && href.startsWith("#")) {
-//       const sectionEl = document.querySelector(href);
-//       sectionEl.scrollIntoView({ behavior: "smooth" });
-//     }
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
 
-//     // Close mobile naviagtion
-//     if (link.classList.contains("main-nav-link"))
-//       headerEl.classList.toggle("nav-open");
-//   });
-// });
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
 
 // ///////////////////////////////////////////////////////////
 // // Sticky navigation
